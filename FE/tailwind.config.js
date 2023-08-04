6d32e43bb97780de87a6c403ffdc81e456ec98e5
dakshayinivtrigent <dakshayini_v@trigent.com>
2023-08-04 11:10:05 +0530

    Added space-mono font to the card

--- FE/tailwind.config.js
+++ FE/tailwind.config.js
@@ -1,26 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}", 
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
+      colors: {
+        "primary-color": "#0064fe",
+        "primary-shade-color": "#00388f",
+        "primary-pale-color": "#e5effe",
+        "primary-highlight-color": "#5c9bff",
+        "text-pale-color": "#e5effe",
+        "scrum-background-color": "#ffffff",
+        "scrum-text-color": "#848b99",
+      },
       animation: {
-        'card-animation': 'card 0.4s',
+        "card-animation": "card 0.4s",
       },
       keyframes: {
         card: {
-          '0%': { transform: 'translateY(-10px)', opacity: '0', backgroundColor: '#0f83f8' },
-          '100%': { transform: 'translateY(0%)', opacity: '100', backgroundColor: '#0f83f8' },
+          "0%": {
+            transform: "translateY(-10px)",
+            opacity: "0",
+            backgroundColor: "#0f83f8",
+          },
+          "100%": {
+            transform: "translateY(0%)",
+            opacity: "100",
+            backgroundColor: "#0f83f8",
+          },
         },
       },
       screens: {
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1280px',
+     
       },
     },
   },
   plugins: [],
-}
+};
