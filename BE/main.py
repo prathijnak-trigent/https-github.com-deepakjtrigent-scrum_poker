857e37d3632031dfe55c2ccd79707938af2f5d9f
saifpashaTrigent <saif_p@trigent.com>
2023-08-04 13:13:27 +0530

    Removed Websockets connection from main.py and data storing file

--- BE/main.py
+++ BE/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from typing import Dict, List
 import uuid
@@ -11,52 +11,13 @@ app = FastAPI()
 rooms_data: Dict[str, Dict[str, List[Dict[str, str]]]] = load_data("rooms_data.json")
 
 class RoomWebSocketManager:
-    def __init__(self):
-        self.active_connections = {}
-
-    async def connect(self, websocket: WebSocket, room_id: str, user_id: str, user_name: str):
-        await websocket.accept()
-        room_key = room_id
-        if room_key in rooms_data:
-            rooms_data[room_key]["users"].append({"userId": user_id, "userName": user_name})
-            self.active_connections[websocket] = {"room_id": room_key, "user_id": user_id}
-            save_data("rooms_data.json", rooms_data)  # Save the updated data
-        else:
-            await websocket.close()
-
-    async def disconnect(self, websocket: WebSocket):
-        if websocket in self.active_connections:
-            room_id = self.active_connections[websocket]["room_id"]
-            user_id = self.active_connections[websocket]["user_id"]
-            room_users = rooms_data.get(room_id, {}).get("users", [])
-            rooms_data[room_id]["users"] = [user for user in room_users if user["userId"] != user_id]
-            del self.active_connections[websocket]
-            save_data("rooms_data.json", rooms_data)  # Save the updated data
-
-    async def send_message(self, websocket: WebSocket, message: dict):
-        await websocket.send_json(message)
-
-websocket_manager = RoomWebSocketManager()
+    pass
 
 class UserWebSocketDetails(BaseModel):
     room_id: str
     user_id: str
     user_name: str
 
-@app.websocket("/ws/")
-async def websocket_endpoint(websocket: WebSocket, user_details: UserWebSocketDetails):
-    await websocket_manager.connect(websocket, user_details.room_id, user_details.user_id, user_details.user_name)
-    try:
-        while True:
-            data = await websocket.receive_json()
-            room_users = rooms_data.get(user_details.room_id, {}).get("users", [])
-            for user in room_users:
-                if user["userId"] != user_details.user_id:
-                    target_websocket = next((ws for ws, info in websocket_manager.active_connections.items() if info["room_id"] == user_details.room_id and info["user_id"] == user["userId"]), None)
-                    if target_websocket:
-                        await websocket_manager.send_message(target_websocket, data)
-    except WebSocketDisconnect:
-        await websocket_manager.disconnect(websocket)
 
 @app.post("/create_room", response_model=Dict[str, str])
 async def create_room():
