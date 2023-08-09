97402dbf7da7dfacf7943f71e3ea38b5dafb08c2
dakshayinivtrigent <dakshayini_v@trigent.com>
2023-08-09 12:15:57 +0530

    Implemented heartbeat

--- FE/src/app/room/room.component.ts
+++ FE/src/app/room/room.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { cardCount } from '../shared/app-data/scrum-points-series';
+import { HeartbeatService } from '../shared/services/heartbeat.service';
 
 @Component({
   selector: 'app-room',
@@ -7,13 +8,18 @@ import { cardCount } from '../shared/app-data/scrum-points-series';
   styleUrls: ['./room.component.css'],
 })
 
-export class RoomComponent {
+export class RoomComponent implements OnInit {
+  constructor(private heartBeat: HeartbeatService) {}
+
+  ngOnInit(): void {
+    this.heartBeat.startHeartbeat();
+  }
 
   public cardCounts: number[] = cardCount;
   public activeIndex: number = -1;
 
   public toggleActive(index: number): void {
     this.activeIndex = this.activeIndex === index ? -1 : index;
+    this.heartBeat.resetHeartbeatTimeout();
   }
-
 }
