6b1dfcaf7345cccbb8d3f4f2f2683e880bb99355
prathijnak-trigent <prathijna_k@trigent.com>
2023-08-10 16:07:42 +0530

    created the intercept and added userid in the headers

--- FE/src/app/user-id.interceptor.ts
+++ FE/src/app/user-id.interceptor.ts
@@ -4,18 +4,20 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
+  HttpHeaders,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { StorageService } from './shared/services/storage.service';
 
 @Injectable()
 export class UserIdInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(private storageService: StorageService) {}
 
   intercept(
     request: HttpRequest<object>,
-    next: HttpHandler): Observable<HttpEvent<object>> {
-    console.log('sending request int');
-    const userId = 'PPDD00';
+    next: HttpHandler
+  ): Observable<HttpEvent<object>> {
+    const userId = this.storageService.user.userId;
 
     const modifiedRequest = request.clone({
       setHeaders: {
