f62d417729b17eb3995dcd0bfd24810769d719e8
saifpashaTrigent <saif_p@trigent.com>
2023-08-11 10:55:39 +0530

    created heartbeat API

--- BE/routers/rooms.py
+++ BE/routers/rooms.py
@@ -7,31 +7,35 @@ from routers.data_manager import load_data, save_data
 
 router = APIRouter()
 
-rooms_data: Dict[str, Dict[str, List[Dict[str, str]]]] = load_data("rooms_data.json")
+rooms_data: Dict[str, Dict[str, List[Dict[str, str]]]
+                 ] = load_data("rooms_data.json")
+
 
 class joinRoomParams(BaseModel):
-    user_id: str
-    user_name: str
+    userId: str
+    userName: str
+
 
 @router.post("/create_room", response_model=Dict[str, str])
 async def create_room():
-        room_id = str(uuid.uuid4())
-        room_key = room_id
-        rooms_data[room_key] = {"users": []}
-        save_data("rooms_data.json", rooms_data)
-        return {"room_id": room_id}
+    room_id = str(uuid.uuid4())
+    room_key = room_id
+    rooms_data[room_key] = {"users": []}
+    save_data("rooms_data.json", rooms_data)
+    return {"room_id": room_id}
+
 
 @router.post("/join_room/{room_id}", response_model=Dict[str, str])
 async def join_room(room_id: str, user_details: joinRoomParams, request: Request):
-        if room_id in rooms_data:
-            rooms_data[room_id]["users"].append({"userId": user_details.user_id, "userName": user_details.user_name})
-            save_data("rooms_data.json", rooms_data)  
-            current_url = str(request.base_url)
-            complete_url = f"{current_url}/join_room/{room_id}"
-            return {
-                "message": f"User {user_details.user_id} joined room {room_id}",
-                "complete_url": complete_url
-            }
-        else:
-            return JSONResponse(status_code=404, content={"error": "Room not found"})
-
+    if room_id in rooms_data:
+        rooms_data[room_id]["users"].append(
+            {"userId": user_details.userId, "userName": user_details.userName})
+        save_data("rooms_data.json", rooms_data)
+        current_url = str(request.base_url)
+        complete_url = f"{current_url}join_room/{room_id}"
+        return {
+            "message": f"User {user_details.userId} joined room {room_id}",
+            "complete_url": complete_url
+        }
+    else:
+        return JSONResponse(status_code=404, content={"error": "Room not found"})
